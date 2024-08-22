baf6ca264cfe77e9c95607a76f9aa44970335928
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2024-08-23 00:29:04 +0300

    Move module_transcriber to separate class

---
+++ module_transcriber.py
@@ -0,0 +1,19 @@
+import time
+
+
+class Transcriber:
+    def __init__(self, file_path, pipe):
+        self.__file_path = file_path
+        self.__pipe = pipe
+
+    def transcribe_file(self):
+        start_time = time.time()
+
+        result = self.__pipe(self.__file_path)  # Transcribe file
+        print(f"File was transcribed.")
+
+        execution_time = round((time.time() - start_time), 2)
+
+        # TODO save TranscribedRecord output_file_path = os.path.join(output_folder, f"{filename}.mp")
+
+        return result, execution_time
